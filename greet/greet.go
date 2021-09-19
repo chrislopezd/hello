6c4ed799923ba5beb8e90f7c1f6221cf5b76973f
Christian Lopez Diaz <christian.lopezdiaz@gmail.com>
2021-09-18 20:31:55 -0500

    Commit inicial

---
+++ greet/greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji = "🙋🏻‍♂️"
+
+//English devuelve saludo en ingles
+func English() string {
+	return "Hi " + emoji
+}
+
+//Italian devuelve saludo en italiano
+func Italian() string {
+	return "Ciao " + emoji
+}
---
+++ greet/spanish.go
@@ -0,0 +1,6 @@
+package greet
+
+//Spanish devuelve saludo en español
+func Spanish() string {
+	return "Hola " + emoji
+}
